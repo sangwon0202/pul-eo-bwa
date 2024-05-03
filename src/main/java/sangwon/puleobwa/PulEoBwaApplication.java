a1e621c88394417280053ab3218fc164a5a5c985
정상원 <topcircle@hanyang.ac.kr>
2024-05-03 14:45:13 +0900

    첫 커밋

---
+++ src/main/java/sangwon/puleobwa/PulEoBwaApplication.java
@@ -0,0 +1,13 @@
+package sangwon.puleobwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulEoBwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulEoBwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sangwon/puleobwa/PulEoBwaApplicationTests.java
@@ -0,0 +1,13 @@
+package sangwon.puleobwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulEoBwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
